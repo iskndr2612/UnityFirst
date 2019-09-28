3199ca7878202d4786989bfc4038487065dbf603
iskndr2612 <iskndr2612@gmail.com>
2019-09-29 00:02:08 +0300

    commm11

---
+++ RandomGen/Assets/ScriptRnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRnd : MonoBehaviour
+{
+    public GameObject[] objects;
+    private GameObject inst_obj;
+
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length - 1);
+        inst_obj = Instantiate(objects[rand], objects[rand].transform.position, Quaternion.identity) as GameObject;
+        inst_obj.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+    }
+
+}
+
