78b4c5ee43512ee614f8dfd0e7ebecb790f9b7a6
iskndr2612 <iskndr2612@gmail.com>
2019-09-29 00:28:56 +0300

    v2

--- RandomGen/Assets/ScriptRnd.cs
+++ RandomGen/Assets/ScriptRnd.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class ScriptRnd : MonoBehaviour
 {
     public GameObject[] objects;
-    private GameObject inst_obj;
+    private GameObject inst_objects;
 
     void Start()
     {
-        int rand = Random.Range(0, objects.Length - 1);
-        inst_obj = Instantiate(objects[rand], objects[rand].transform.position, Quaternion.identity) as GameObject;
-        inst_obj.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        int rand = Random.Range(0, objects.Length);
+        inst_objects = Instantiate(objects[rand], objects[rand].transform.position, Quaternion.identity) as GameObject;
+        inst_objects.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+       // Invoke("Inst", 2f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+            StartCoroutine(instObj());
+    }
+
+    void Inst()
+    {
+        Instantiate(inst_objects, new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+    }
+
+    IEnumerator instObj()
+    {
+        int rand2 = Random.Range(0, objects.Length);
+        Instantiate(objects[rand2], new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+        yield return new WaitForSeconds(1.5f);
+       
     }
 
 }
-
